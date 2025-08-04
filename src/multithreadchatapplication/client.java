fc5cde781415108f28f5249ee3168ef21b1367ab
Diva-28 <divakar2877@gmail.com>
2025-08-04 19:12:37 +0530

    Update client.java

--- src/multithreadchatapplication/client.java
+++ src/multithreadchatapplication/client.java
@@ -1,4 +1,31 @@
-package multithreadchatapplication;
+package multithreadchatapplication_project;
 
-public class client {
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 4321);
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        // Read messages from server
+        new Thread(() -> {
+            String msg;
+            try {
+                while ((msg = in.readLine()) != null) {
+                    System.out.println(msg);
+                }
+            } catch (IOException e) {
+                System.out.println("Disconnected.");
+            }
+        }).start();
+
+        // Send messages to server
+        String input;
+        while ((input = userInput.readLine()) != null) {
+            out.println(input);
+        }
+    }
 }
